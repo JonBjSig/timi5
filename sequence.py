74b55aed78f5fc06c041a170fdfc26ee9fe5bc1d
JonBjSig <jaunanb@gmail.com>
2020-09-03 10:21:20 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,6 @@
+Take in a value n as input
+The program holds the last 3 values and the curret calculated sum (all initialized as 0)
+The program then performs the following operation n times
+Calculate the sum of the last 3 values and store in the sum variable
+Each past value is then shifted forward, setting the latest value as equal to the sum variable and dropping the oldest value
+Print the sum variable
