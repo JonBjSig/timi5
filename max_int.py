bd9d23661bd5dc2aeeacc704a8a835d99d55eb5d
JonBjSig <jaunanb@gmail.com>
2020-09-03 09:53:31 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,5 @@
+first take a number as input from the user
+set the max_int variable to that value
+continually ask the user to input new numbers
+if the new input is larger than the max_int variable, set the max_int variable to that value
+once the user inputs a negative number, the program stops and prints the largest value the user typed in
